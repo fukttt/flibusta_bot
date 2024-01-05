fd44485ea8cf889459db66384d9b3607bee07bd5
rmrf <you@example.com>
2024-01-05 20:35:56 +0800

    Added some functionality

--- main.py
+++ main.py
@@ -2,56 +2,75 @@ import asyncio
 import logging
 import sys
 from os import getenv
-from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
-
+from utils.flibusta_crawler import Flibusta, Flibusta_Book
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from dotenv import load_dotenv
 
 load_dotenv()
 
-# Bot token can be obtained via https://t.me/BotFather
 TOKEN = getenv("BOT_TOKEN")
 
-# All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    """
-    This handler receives messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    kb = [
+        [types.KeyboardButton(text="Поиск книги 📖")],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+
+    await message.answer(
+        f"Привет, {hbold(message.from_user.full_name)}!\nРад приветствовать тебя в нашем боте Flibusta Library.",
+        reply_markup=keyboard,
+    )
+
+
+@dp.callback_query()
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer(callback.data)
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
     try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
+        if "Поиск книги" in message.text:
+            await message.answer("Для того чтобы найти книгу напиши ее название мне.")
+        else:
+            ff = Flibusta(9052, "127.0.0.1")
+            if await ff.check_connection():
+                books = await ff.search_for_books(query=message.text)
+
+                builder = InlineKeyboardBuilder()
+                for book in books:
+                    builder.row(
+                        types.InlineKeyboardButton(
+                            text=f"📖 {book.name} - {book.author}",
+                            callback_data="suka",
+                        )
+                    )
+                if len(books) == 0:
+                    await message.answer(
+                        "не могу найти запрошенную книгу. Прошу прощения."
+                    )
+                else:
+                    await message.answer(
+                        "Найденные книги: ", reply_markup=builder.as_markup()
+                    )
+            else:
+                await message.answer("Что-то не так")
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
         await message.answer("Nice try!")
 
 
 async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
